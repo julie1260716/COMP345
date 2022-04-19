ead24282730e3f4340f9d8dd3d2a078ad66e7090
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-19 19:13:53 +0800

    Delete card.cpp

--- card.cpp
+++
@@ -1,6 +0,0 @@
-#include "card.h"
-
-ostream& operator<<(ostream& os, const Card& card){
-    os << "Name:" << card.name << "\tType:" << card.type << "\tEffect:" << card.effect << endl;
-    return os;
-}
